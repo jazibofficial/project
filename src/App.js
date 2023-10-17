2ab6a1f8537e15de5d2892f11bd5dea173e79d6c
Muhammad jazib ali <jazibalisher@gmail.com>
2023-10-17 03:03:56 -0700

    First Upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+// every tag must be closed 
+
+
+
+
+function App() {
+  return (
+
+    <img src="jazib.jp."  width="500" height="600"/>
+
+ 
+  );
+}
+
+
+
+
+
+export default App;
